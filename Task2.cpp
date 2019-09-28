40b90bb0cecff0c1b0805141ece8e6091a40d6c1
Nick-vdM <nicolaas.vdm1@gmail.com>
2019-09-28 21:22:32 +1000

    1 & 2 functional
    Took 2 hours 37 minutes

--- Task2.cpp
+++ Task2.cpp
@@ -5,6 +5,9 @@
 #include <array>
 #include <limits>
 #include <stack>
+#include <fstream>
+#include <vector>
+#include <queue>
 
 
 /*
@@ -17,41 +20,55 @@
  *      - Number of nodeCount popped from the stack
  *      - Time taken to find the shortest path
  *
- * - Iterative search: stack
- * - Breadth first search: queue
- * - Uniform cost search: priority queue
+ * - Iterative Search: stack
+ * - Breadth first Search: queue
+ * - Uniform cost Search: priority queue
  *
  * 1 mark: implementing required data structures
- * 2 marks: implementing one search algorithm
+ * 2 marks: implementing one Search algorithm
  * 1 mark: statistics collected
  * 1 mark: other two algorithms
  */
 
 template<typename T>
-class search {
+class Search {
 public:
-    void createGraph(size_t graphSize) {
+    Search(int source, int target)
+            : source(source), target(target) {
+        std::ifstream ifs("graph100.txt");
+        ifs >> nodeCount;
+        for (int i = 0; i < nodeCount; i++) {
+            matrix.emplace_back();
+            for (int j = 0; j < nodeCount; j++) {
+                matrix[i].push_back(0);
+            }
+        }
 
+        for (int i = 0; i < nodeCount; i++) {
+            for (int j = 0; j < nodeCount; j++) {
+                ifs >> matrix[i][j];
+            }
+        }
     }
+
 
     void doSearch() {
         clock_t startClock = clock();
-
-        std::array<size_t, nodeCount> parent;
-        std::array<size_t, nodeCount> cost;
-        cost.fill(std::numeric_limits<size_t>::max());
+        std::vector<int> parent(7, -1);
+        std::vector<int> cost(nodeCount, std::numeric_limits<short>::max());
+        cost[source] = 0;
 
         heap.push(source);
-        size_t current; // Don't replace the space over and over
-        while(!heap.empty()){
-            current = heap.pop();
+        int current; // Don't replace the space over and over
+        while (!heap.empty()) {
+            current =  popNodeFromHeap(heap);
             nodesPopped++;
 
-            if(current != target){
-                for(int neigh = 0; neigh < nodeCount; neigh++){
-                    if(graph[current][neigh] == 0) continue; // Skip no links
-                    if (cost[neigh] > cost[current] + graph[current][neigh]){
-                        cost[neigh] = cost[current] + graph[current][neigh];
+            if (current != target) {
+                for (int neigh = 0; neigh < nodeCount; neigh++) {
+                    if (matrix[current][neigh] == 0) continue; // Skip no links
+                    if (cost[neigh] > cost[current] + matrix[current][neigh]) {
+                        cost[neigh] = cost[current] + matrix[current][neigh];
                         parent[neigh] = current;
                         heap.push(neigh);
                         nodesPushed++;
@@ -59,35 +76,87 @@ public:
                 }
             }
         }
-        timeTaken = (clock() - startClock) / CLOCKS_PER_SEC;
+        shortestPathLength = cost[target];
+        setShortestPath(parent);
+        timeTaken = double((clock() - startClock)) / CLOCKS_PER_SEC;
+    }
+
+    void setShortestPath(std::vector<int> &parent) {
+        // The parent is stored such from the destination the index points
+        // towards the previous node
+        int current = target;
+        while (current != source) {
+            shortestPath.insert(shortestPath.begin(), current);
+            current = parent[current];
+            hopsOnShortestPath++;
+        }
+        shortestPath.insert(shortestPath.begin(), current);
     }
 
     void printData() {
-        std::cout << "Shortest path: " << shortestPath << std::endl
-                  << "Number of hops on shortest path: " << hopsOnShortestPath << std::end
+        std::cout << "Shortest path: ";
+        printPath();
+        std::cout << std::endl
+                  << "Number of hops on shortest path: " << hopsOnShortestPath << std::endl
                   << "Shortest path length: " << shortestPathLength << std::endl
                   << "Nodes added: " << nodesPushed << std::endl
-                  << "Nodes popped: " << nodesPopped << std::endl;
-    };
+                  << "Nodes popped: " << nodesPopped << std::endl
+                  << "Time taken: " << timeTaken << std::endl;
+    }
 
 private:
-    size_t source;
-    size_t target;
+    void printPath() {
+        for (auto i : shortestPath) {
+            std::cout << i << " ";
+        }
+    }
 
-    std::stack<size_t> heap;
-    static size_t nodeCount;
-    std::array<std::array<size_t, nodeCount>, nodeCount> graph;
+    int popNodeFromHeap(std::queue<int> & queueMode) {
+        //Due to structures having different pop syntaxes a switch is needed here
+        auto value = queueMode.front();
+        heap.pop();
+        return value;
+    }
 
-    std::string shortestPath;
-    size_t hopsOnShortestPath = 0;
-    size_t shortestPathLength = std::numeric_limits<size_t>::max();
-    size_t nodesPushed = 0;
-    size_t nodesPopped = 0;
+    int popNodeFromHeap(std::stack<int> & stackMode){
+        auto value = stackMode.top();
+        heap.pop();
+        return value;
+    }
+
+    int popNodeFromHeap(std::priority_queue<int> & priorityQueueMode){
+        auto value = priorityQueueMode.top();
+        heap.pop();
+        return value;
+    }
+
+    int source;
+    int target;
+    int nodeCount;
+
+    T heap;
+    std::vector<std::vector<int>> matrix;
+
+    std::vector<int> shortestPath;
+    int hopsOnShortestPath = 0;
+    int shortestPathLength = std::numeric_limits<short>::max();
+    int nodesPushed = 0;
+    int nodesPopped = 0;
     double timeTaken;
-
 };
 
+
 int main() {
-
-
+    std::cout << "==============STACK============" << std::endl;
+    Search<std::stack<int>> search_s{0, 4};
+    search_s.doSearch();
+    search_s.printData();
+    std::cout << "==============QUEUE============" << std::endl;
+    Search<std::queue<int>> search_q{0, 4};
+    search_q.doSearch();
+    search_q.printData();
+    std::cout << "==============PRIORITY QUEUE============" << std::endl;
+    Search<std::priority_queue<int>> search_pq{0, 4};
+    search_pq.doSearch();
+    search_pq.printData();
 }
